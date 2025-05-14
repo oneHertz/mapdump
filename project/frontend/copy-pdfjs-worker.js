4cc5a60b407816ea9a3143e5153910bef4da7154
Raphaël Stefanini <raphael.stefanini@ecom.fi>
2025-05-14 14:24:48 +0300

    pdf lib updgrade

---
+++ project/frontend/copy-pdfjs-worker.js
@@ -0,0 +1,7 @@
+import path from 'node:path';
+import fs from 'node:fs';
+
+const pdfjsDistPath = path.dirname(import.meta.resolve('pdfjs-dist/package.json'));
+const pdfWorkerPath = path.join(pdfjsDistPath, 'build', 'pdf.worker.mjs');
+
+fs.copyFileSync(pdfWorkerPath.substring(5), 'public/pdf.worker.mjs');
