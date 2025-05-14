dfedc1fb3a93d7bd3bab90dec7c5935d94c041af
Raphael Stefanini <raphael@rphlo.com>
2025-05-14 18:08:19 +0300

    Precommit

--- project/frontend/copy-pdfjs-worker.mjs
+++ project/frontend/copy-pdfjs-worker.mjs
@@ -1,7 +1,9 @@
-import path from 'node:path';
-import fs from 'node:fs';
+import path from "node:path";
+import fs from "node:fs";
 
-const pdfjsDistPath = path.dirname(import.meta.resolve('pdfjs-dist/package.json'));
-const pdfWorkerPath = path.join(pdfjsDistPath, 'build', 'pdf.worker.mjs');
+const pdfjsDistPath = path.dirname(
+  import.meta.resolve("pdfjs-dist/package.json")
+);
+const pdfWorkerPath = path.join(pdfjsDistPath, "build", "pdf.worker.mjs");
 
-fs.copyFileSync(pdfWorkerPath.substring(5), 'public/pdf.worker.js');
+fs.copyFileSync(pdfWorkerPath.substring(5), "public/pdf.worker.js");
