095be4432a2a055373456855d347c3c0e39354ba
Raphael Stefanini <raphael@rphlo.com>
2025-06-24 14:51:59 +0300

    json update

--- project/frontend/src/components/Login.jsx
+++ project/frontend/src/components/Login.jsx
@@ -52,7 +52,7 @@ const Login = () => {
     } else {
       setWantLogin(false);
       const json = await res.json();
-      globalState.setUser({ username: json.username, api_token: json.token });
+      globalState.setUser({ username: json.username, api_token: json.key });
     }
   };
   const onLogout = async () => {
