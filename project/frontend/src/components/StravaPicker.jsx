113fb0dd4a109d9ea29e11d70353275c025a6ce1
Rapha <rphlo@users.noreply.github.com>
2025-08-24 21:34:57 +0300

    cc

--- project/frontend/src/components/StravaPicker.jsx
+++ project/frontend/src/components/StravaPicker.jsx
@@ -138,7 +138,7 @@ const Settings = (props) => {
         description: act.description,
       });
     } catch (e) {
-      throw Error(e);
+      throw new Error(e);
       Swal.fire({
         title: "Error!",
         text: "Could not import this activity!",
