b360a8e8afbaf17f52f40b0e97c924f6bae2d178
Raphael Stefanini <raphael@rphlo.com>
2025-09-11 14:25:18 +0300

    Fix

--- project/settings.py
+++ project/settings.py
@@ -174,7 +174,7 @@ AUTHENTICATION_BACKENDS = ("project.utils.backends.CaseInsensitiveModelBackend",
 SITE_ID = 1
 
 REST_AUTH = {
-    "REGISTER_SERIALIZER": "project.utils.serializers.RegisterSerializer",
+    #"REGISTER_SERIALIZER": "project.utils.serializers.RegisterSerializer",
     #"PASSWORD_RESET_SERIALIZER": "project.utils.serializers.CustomPasswordResetSerializer",
 }
 
