418c1f09fa0c8eb8b1abc2e87f0a05ab2a904249
Raphael Stefanini <raphael@rphlo.com>
2025-09-11 13:58:28 +0300

    Fix

--- project/utils/serializers.py
+++ project/utils/serializers.py
@@ -30,7 +30,7 @@ class RegisterSerializer(serializers.Serializer):
     email = serializers.EmailField(required=allauth_settings.EMAIL_REQUIRED)
     password1 = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True)
-
+    _has_phone_field = False
     def validate_username(self, username):
         username = get_adapter().clean_username(username)
         return username
